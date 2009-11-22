2d3958c27ca4bca827d264b5ebaa2efec8e917af
fengmk2 <fengmk2@8cb29146-d722-11de-9196-2db6e9c7c7ae>
2009-11-22 09:24:23 +0000

    Implementation coroutine trampolining, detail please see ../examples/trampolining.py

--- examples/echoserver.py
+++ examples/echoserver.py
@@ -8,6 +8,21 @@ from litchi.schedule import Scheduler
 from litchi.systemcall import ReadWait, NewTask, WriteWait
 
 
+def accept(sock):
+    yield ReadWait(sock)
+    yield sock.accept()
+    
+def send(sock, buffer):
+    while buffer:
+        yield WriteWait(sock)
+        len = sock.send(buffer)
+        buffer = buffer[len:]
+        
+def recv(sock, maxbytes):
+    yield ReadWait(sock)
+    yield sock.recv(maxbytes)
+        
+
 clients = []
 
 def send_to(client, data):
@@ -17,16 +32,18 @@ def send_to(client, data):
 def handle_client(client, addr):
     print 'Connection from %s %s' % addr
     while True:
-        yield ReadWait(client)
-        data = client.recv(65536)
+#        yield ReadWait(client)
+#        data = client.recv(65536)
+        data = yield recv(client, 65536)
         data = data.strip()
         if not data:
             break
         data = '[%s %s] %s\r\n' % (addr[0], addr[1], data)
         for other in clients:
             if other != client:
-                yield WriteWait(other)
-                other.send(data)
+                yield send(other, data)
+#                yield WriteWait(other)
+#                other.send(data)
 #                yield NewTask(send_to(other, data))
 #        yield WriteWait(client)
 #        client.send(`data`)
@@ -44,8 +61,9 @@ def server(port):
     sock.listen(10)
     try:
         while True:
-            yield ReadWait(sock)
-            client, addr = sock.accept()
+#            yield ReadWait(sock)
+#            client, addr = sock.accept()
+            client, addr = yield accept(sock)
             clients.append(client)
             yield NewTask(handle_client(client, addr))
     finally:
