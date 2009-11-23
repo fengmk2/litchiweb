eb28e77a7c51f04521163c69fcc2ef22f942634f
fengmk2 <fengmk2@8cb29146-d722-11de-9196-2db6e9c7c7ae>
2009-11-23 06:18:16 +0000

    add Sleep call; handle task close

--- litchi/io.py
+++ litchi/io.py
@@ -7,36 +7,63 @@ import select
 
 class EventHub(object):
     
+    # Constants from the epoll module
+    _EPOLLIN = 0x001
+    _EPOLLPRI = 0x002
+    _EPOLLOUT = 0x004
+    _EPOLLERR = 0x008
+    _EPOLLHUP = 0x010
+    _EPOLLRDHUP = 0x2000
+    _EPOLLONESHOT = (1 << 30)
+    _EPOLLET = (1 << 31)
+
+    # Our events map exactly to the epoll events
+    NONE = 0
+    READ = _EPOLLIN
+    WRITE = _EPOLLOUT
+    ERROR = _EPOLLERR | _EPOLLHUP | _EPOLLRDHUP
+    
     def __init__(self):
-        self.read_waiting = set()
-        self.write_waiting = set()
+        self.fds = {}
         
     def poll(self, timeout=0):
         raise NotImplementedError()
     
-    def register_read(self, fd):
-        self.read_waiting.add(fd)
+    def register(self, fd, events):
+        self.fds[fd] = events
         
-    def unregister_read(self, fd):
-        self.read_waiting.remove(fd)
+    def unregister(self, fd):
+        del self.fds[fd]
+            
+#    def register_read(self, fd):
+#        self.read_waiting.add(fd)
+#        
+#    def unregister_read(self, fd):
+#        if fd in self.read_waiting:
+#            self.read_waiting.remove(fd)
+#            return True
+#        return False
+#    
+#    def register_write(self, fd):
+#        self.write_waiting.add(fd)
+#        
+#    def unregister_write(self, fd):
+#        if fd in self.write_waiting:
+#            self.write_waiting.remove(fd)
+#            return True
+#        return False
     
-    def register_write(self, fd):
-        self.write_waiting.add(fd)
-        
-    def unregister_write(self, fd):
-        self.write_waiting.remove(fd)
     
-    
-class SelectEventHub(EventHub):
-    
-    def poll(self, timeout=0):
-        r, w, _ = select.select(self.read_waiting, self.write_waiting, [], timeout)
-        return r, w
+#class SelectEventHub(EventHub):
+#    
+#    def poll(self, timeout=0):
+#        r, w, _ = select.select(self.read_waiting, self.write_waiting, [], timeout)
+#        return r, w
     
     
 class EPollEventHub(EventHub):
-    '''A epoll-based hub.
-    '''
+    """A epoll-based hub."""
+    
     SIZE_HINT = 50000
     def __init__(self):
         super(EPollEventHub, self).__init__()
@@ -45,35 +72,47 @@ class EPollEventHub(EventHub):
     def poll(self, timeout=0):
         if timeout is None:
             timeout = -1
-        events = self.epoll.poll(timeout)
-        r = (fd for fd, event in events if event in (select.EPOLLIN, select.EPOLLPRI)) # read ready
-        w = (fd for fd, event in events if event == select.EPOLLOUT) # write ready
-        return r, w
+        return self.epoll.poll(timeout)
+#        r, w, e = [], [], []
+#        for fd, events in eventpairs:
+#            if events & self.READ: # read ready
+#                r.append(fd)
+#            if events & self.WRITE: # write ready
+#                w.append(fd)
+#            if events & self.ERROR: # write ready
+#                e.append(fd)
+#                print fd, '%x' % events
+#        return r, w, e
     
-    def register_read(self, fd):
-        if fd not in self.read_waiting and fd not in self.write_waiting:
-            self.epoll.register(fd, select.EPOLLIN | select.EPOLLPRI)
-        super(EPollEventHub, self).register_read(fd)
-        
-    def register_write(self, fd):
-        if fd not in self.write_waiting:
-            if fd in self.read_waiting:
-                self.epoll.modify(fd, select.EPOLLIN | select.EPOLLPRI | select.EPOLLOUT)
-            else:
-                self.epoll.register(fd, select.EPOLLIN | select.EPOLLPRI | select.EPOLLOUT)
-                self.read_waiting.add(fd)
-        super(EPollEventHub, self).register_write(fd)
-    
-    def unregister_read(self, fd):
-        super(EPollEventHub, self).unregister_read(fd)
-        self.epoll.unregister(fd)
-        
-    def unregister_write(self, fd):
-        super(EPollEventHub, self).unregister_write(fd)
-        if fd in self.read_waiting:
-            self.epoll.modify(fd, select.EPOLLIN | select.EPOLLPRI)
+    def register(self, fd, events):
+        if fd not in self.fds:
+            self.epoll.register(fd, events | self.ERROR)
         else:
-            self.epoll.unregister(fd)
+            self.epoll.modify(fd, events | self.ERROR)
+        super(EPollEventHub, self).register(fd, events)
+        
+    def unregister(self, fd):
+        super(EPollEventHub, self).unregister(fd)
+        self.epoll.unregister(fd)
+
+#    def register_write(self, fd):
+#        if fd not in self.read_waiting and fd not in self.write_waiting:
+#            self.epoll.register(fd, self.WRITE | self.ERROR)
+#        else:
+#            self.epoll.modify(fd, self.WRITE | self.ERROR)
+#        super(EPollEventHub, self).register_write(fd)
+#    
+#    def unregister_read(self, fd):
+#        if super(EPollEventHub, self).unregister_read(fd):
+#            self.epoll.unregister(fd)
+#            return True
+#        return False
+#        
+#    def unregister_write(self, fd):
+#        if super(EPollEventHub, self).unregister_write(fd):
+#            self.epoll.unregister(fd)
+#            return True
+#        return False
         
         
 def get_hub():
