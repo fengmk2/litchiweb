2d8e55336b10f3c4bf64f504de2db262e0acdb55
fengmk2 <fengmk2@8cb29146-d722-11de-9196-2db6e9c7c7ae>
2009-11-24 05:52:16 +0000

    support cookies

--- trunk/litchi/http.py
+++ trunk/litchi/http.py
@@ -10,6 +10,10 @@ import urlparse
 import time
 import logging
 import httplib
+import Cookie
+import datetime
+import calendar
+import email.utils
 
 from litchi.socketwrap import Socket
 from litchi.systemcall import NewTask
@@ -286,6 +290,11 @@ class HTTPHeaders(dict):
                 headers[name] = value
         return headers
 
+def _utf8(s):
+    if isinstance(s, unicode):
+        return s.encode("utf-8")
+    assert isinstance(s, str)
+    return s
 
 class HTTPReponse(object):
     """HTTP Reponse"""
@@ -304,7 +313,6 @@ class HTTPReponse(object):
         if self.content_type.startswith('text/'):
             self.headers['Content-Type'] = '%s; charset=%s' % (self.content_type, self.default_charset)
         self.request = request
-        self.cookies = None
         
     def format(self):
         # protocol, status
@@ -318,10 +326,60 @@ class HTTPReponse(object):
             self.headers['Content-Length'] = len(self.body)
         datas.extend(['\r\n%s: %s' % (n, v) for n, v in self.headers.iteritems()])
         # cookies
-#        for cookie_dict in getattr(self, "_new_cookies", []):
-#            for cookie in cookie_dict.values():
-#                lines.append("Set-Cookie: " + cookie.OutputString(None))
+        for cookie_dict in getattr(self, "_new_cookies", []):
+            for cookie in cookie_dict.values():
+                datas.append("\r\nSet-Cookie: %s" % cookie.OutputString())
         datas.append('\r\n\r\n')
         # body
         datas.append(self.body)
         return ''.join(datas)
+    
+    @property
+    def cookies(self):
+        """A dictionary of Cookie.Morsel objects."""
+        if not hasattr(self, "_cookies"):
+            self._cookies = Cookie.BaseCookie()
+            if "Cookie" in self.request.headers:
+                try:
+                    self._cookies.load(self.request.headers["Cookie"])
+                except:
+                    self.clear_all_cookies()
+        return self._cookies
+
+    def get_cookie(self, name, default=None):
+        """Gets the value of the cookie with the given name, else default."""
+        if name in self.cookies:
+            return self.cookies[name].value
+        return default
+
+    def set_cookie(self, name, value, path="/", domain=None, expires=None, expires_days=None):
+        """Sets the given cookie name/value with the given options."""
+        name = _utf8(name)
+        value = _utf8(value)
+#        if re.search(r"[\x00-\x20]", name + value):
+#            # Don't let us accidentally inject bad stuff
+#            raise ValueError("Invalid cookie %r: %r" % (name, value))
+        if not hasattr(self, "_new_cookies"):
+            self._new_cookies = []
+        new_cookie = Cookie.BaseCookie()
+        self._new_cookies.append(new_cookie)
+        new_cookie[name] = value
+        if domain:
+            new_cookie[name]["domain"] = domain
+        if expires_days is not None and not expires:
+            expires = datetime.datetime.utcnow() + datetime.timedelta(days=expires_days)
+        if expires:
+            timestamp = calendar.timegm(expires.utctimetuple())
+            new_cookie[name]["expires"] = email.utils.formatdate(timestamp, localtime=False, usegmt=True)
+        if path:
+            new_cookie[name]["path"] = path
+
+    def clear_cookie(self, name, path="/", domain=None):
+        """Deletes the cookie with the given name."""
+        expires = datetime.datetime.utcnow() - datetime.timedelta(days=365)
+        self.set_cookie(name, value="", path=path, domain=domain, expires=expires)
+
+    def clear_all_cookies(self):
+        """Deletes all the cookies the user sent with this request."""
+        for name in self.cookies.iterkeys():
+            self.clear_cookie(name)
